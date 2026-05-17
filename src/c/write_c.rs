41cd78f5c1028a8df7c1fb754c3c1bfe16cd8f83
punn lertjaturaphat <punnlertjaturaphat@gmail.com>
2026-05-17 23:45:42 +0900

    partial implemented writeline
    for translation unit, declaration

--- src/c/write_c.rs
+++ src/c/write_c.rs
@@ -20,6 +20,33 @@ impl<T: WriteString> WriteString for Node<T> {
 impl WriteLine for TranslationUnit {
     /// VERY BIG HINT: You should start by understanding the [`writeln!`](https://doc.rust-lang.org/std/macro.writeln.html) macro.
     fn write_line(&self, indent: usize, write: &mut dyn Write) -> Result<()> {
+        // since TranslationUnit is a vec of node of external declaration, the first field (self.0)
+        // is the vec
+        for ext_decl in &self.0 {
+            ext_decl.write_line(indent, write);
+            writeln!(write)?;
+        }
+        Ok(())
+    }
+}
+
+impl WriteLine for ExternalDeclaration {
+    fn write_line(&self, indent: usize, write: &mut dyn Write) -> Result<()> {
+        match self {
+            ExternalDeclaration::Declaration(decl) => decl.write_line(indent, write),
+            ExternalDeclaration::StaticAssert(_) => panic!("doesn't support static assert"),
+            ExternalDeclaration::FunctionDefinition(fdef) => fdef.write_line(indent, write),
+        }
+    }
+}
+
+impl WriteLine for Declaration {
+    fn write_line(&self, indent: usize, write: &mut dyn Write) -> Result<()> {
+        write_indent(indent, write)?;
+        writeln!(write, "{};", self.write_string())?;
+        Ok(())
+    }
+}
         todo!()
     }
 }
