647cd4fbb7aaf7561910ed33e1ef8743f12af2ac
punn lertjaturaphat <punnlertjaturaphat@gmail.com>
2026-05-25 12:59:38 +0900

    implemented the alignof translation

--- src/irgen/mod.rs
+++ src/irgen/mod.rs
@@ -1397,7 +1397,21 @@ impl IrgenFunc<'_> {
                 )))
             }
             Expression::SizeOfVal(node) => todo!(),
-            Expression::AlignOf(node) => todo!(),
+            Expression::AlignOf(type_name) => {
+                let dtype = ir::Dtype::try_from(&type_name.node.0.node)
+                    .map_err(|e| IrgenErrorMessage::InvalidDtype { dtype_error: e })?;
+                let (_, align_of) = dtype
+                    .size_align_of(self.structs)
+                    .map_err(|e| IrgenErrorMessage::InvalidDtype { dtype_error: e })?;
+
+                // TODO: `is_signed` must be false in the future (unsigned)
+                // [UPDATE] i found the set_signed function in ir Dtype. i think this is the right
+                // way to set it to unsigned
+                Ok(ir::Operand::Constant(ir::Constant::int(
+                    align_of as u128,
+                    ir::Dtype::LONG.set_signed(false),
+                )))
+            }
             Expression::UnaryOperator(node) => todo!(),
             Expression::Cast(expr) => {
                 let target_dtype = ir::Dtype::try_from(&expr.node.type_name.node)
