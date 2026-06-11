01235788d1f147fb805b4c0948319f58ce6af7dd
punn lertjaturaphat <punnlertjaturaphat@gmail.com>
2026-06-11 09:44:32 +0900

    fix return operand of member expr

--- src/irgen/mod.rs
+++ src/irgen/mod.rs
@@ -2476,14 +2476,7 @@ impl IrgenFunc<'_> {
                             return Ok(field);
                         }
 
-                        if dtype.get_struct_name().is_some() {
-                            return Ok(field);
-                        }
-
                         if let Some(array_inner) = dtype.get_array_inner() {
-                            // [SELF] not sure how to do this
-                            // maybe recursion to see if the inner is array. go until not array
-                            // use the pointer at the last. but is it ok?
                             return self.translate_array_pointer(&field, array_inner, context);
                         }
                         context.insert_instruction(ir::Instruction::Load { ptr: field })
@@ -2527,8 +2520,19 @@ impl IrgenFunc<'_> {
                                 offset as u128,
                                 ir::Dtype::LONG,
                             )),
-                            dtype: ir::Dtype::pointer(dtype),
+                            dtype: ir::Dtype::pointer(dtype.clone()),
                         })?;
+                        if let Some(array_inner) = dtype.get_array_inner() {
+                            // [SELF] not sure how to do this
+                            // maybe recursion to see if the inner is array. go until not array
+                            // use the pointer at the last. but is it ok?
+                            return self.translate_array_pointer(&field, array_inner, context);
+                        }
+
+                        if dtype.get_function_inner().is_some() {
+                            return Ok(field);
+                        }
+
                         context.insert_instruction(ir::Instruction::Load { ptr: field })
                     }
                 }
